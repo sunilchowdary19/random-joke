7fc1964840d69485dfe28e904e5ba3390de78ed3
sunilchowdary19 <sunilchowdarymatukumilli@gmail.com>
2024-02-20 22:29:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+const getJoke = () => {
+    jokeContainer.classList.remove("fade");
+    fetch(url)
+    .then(data => data.json())
+    .then(item =>{
+        jokeContainer.textContent = `${item.joke}`;
+        jokeContainer.classList.add("fade");
+    })
+    .catch(error => {
+        console.error("Error fetching joke:", error);
+    })};
+
+// const getJoke = async () => {
+//     try {
+//         const response = await fetch('https://reqres.in/api/users/');
+//         const data = await response.json();
+//         const randomIndex = Math.floor(Math.random() * data.data.length);
+//         const randomEmail = data.data[randomIndex].email;
+//         jokeContainer.textContent = randomEmail;
+//         jokeContainer.classList.add("fade");
+//     } catch (error) {
+//         console.error("Error fetching joke:", error);
+//     }
+// };
+btn.addEventListener("click",getJoke);
+getJoke();
